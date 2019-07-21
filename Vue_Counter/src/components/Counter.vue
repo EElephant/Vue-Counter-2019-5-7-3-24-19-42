938213c3df78dafa87f70b68cedb3aaedbb48a8a
Ethan <ethan.huang@oocl.com>
2019-07-21 21:03:05 +0800

    Feat: add Counter Component and apply to App.vue

---
+++ Vue_Counter/src/components/Counter.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="Counter">
+        <table>
+            <ul id="counterNum">{{counter}}</ul>
+            <ul>
+                <button @click="addCounter">点我+</button>
+                <button @click="reduceCounter">点我-</button>
+            </ul>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+        name: "Counter",
+        data:function(){
+            return{
+                counter:0
+            }
+        },
+        methods:{
+            addCounter:function () {
+                this.counter ++;
+                
+            },
+            reduceCounter:function () {
+                this.counter --;
+                
+            }
+        },
+    }
+</script>
+
+<style scoped>
+#Counter {
+    /*align-content: center;*/
+    /*width: 200px;*/
+    margin: auto;
+}
+</style>
